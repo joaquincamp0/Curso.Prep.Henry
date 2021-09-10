c79e26d565b8c895e65f4806e0d6d71cc344e898
Pablo Joaquin Campo <joaquincampo@outlook.com>
2021-09-10 03:30:19 -0300

    repasosfinales

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -67,16 +67,11 @@ function colors(color) {
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
   switch(color) {
-    case 'blue' : 
-    return 'This is blue'
-    case 'red' : 
-       return 'This is red'
-    case 'green' : 
-       return 'This is green'
-    case 'orange' : 
-       return 'This is orange'
-    default : 
-       return 'Color not found'
+    case 'blue' : return 'This is blue'
+    case 'red' : return 'This is red';
+    case 'green' : return 'This is green';
+    case 'orange' : return 'This is orange';
+    default : return 'Color not found';
   }
 }
 
